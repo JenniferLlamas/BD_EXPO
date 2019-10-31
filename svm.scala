8d2378f157e8d59dc753800e6295e6ea533e9c76
armando0509 <38328701+armando0509@users.noreply.github.com>
2019-10-30 22:23:20 -0700

    Comments were added to the lines of the code

--- svm.scala
+++ svm.scala
@@ -1,8 +1,12 @@
+
+// The library needed to use the linear support vector machine qualifier is imported
 import org.apache.spark.ml.classification.LinearSVC
 
 // Load training data
 val training = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
 
+
+// An Lsvc variable was created where the .setMax (10) and .setRegParam (0.1) parameters are set
 val lsvc = new LinearSVC()
   .setMaxIter(10)
   .setRegParam(0.1)
