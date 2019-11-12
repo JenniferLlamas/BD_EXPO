cd3556e95fa743954b0f8278416af5f3c309df7a
armando0509 <38328701+armando0509@users.noreply.github.com>
2019-11-12 06:12:00 +0100

    Actualizar lineas de codigo en svm.scala

--- svm.scala
+++ svm.scala
@@ -1,8 +1,9 @@
 
-// The library needed to use the linear support vector machine qualifier is imported
+//To start using the linear support vector machine algorithm, the following algorithm libraries need to be set the library needed to use the linear support vector machine qualifier is imported
 import org.apache.spark.ml.classification.LinearSVC
 
-// Load training data
+// The proposed training data is loaded to start using the algorithm
+
 val training = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
 
 
@@ -11,8 +12,10 @@ val lsvc = new LinearSVC()
   .setMaxIter(10)
   .setRegParam(0.1)
 
-// Fit the model
+// The model is adjusted to use the training
+
 val lsvcModel = lsvc.fit(training)
 
-// Print the coefficients and intercept for linear svc
+// // Subsequently here the Coefficients are printed and intercepted for linear svc and once executed in the console it shows us the final result
+
 println(s"Coefficients: ${lsvcModel.coefficients} Intercept: ${lsvcModel.intercept}")
